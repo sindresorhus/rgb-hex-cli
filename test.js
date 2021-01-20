ee34803aac8b58a1318ffe7355527974da53c450
Matt Jarrett <cujarrett@gmail.com>
2021-01-19 23:05:11 -0600

    fix(tests): updates test syntax based on dependencies

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
-import test from 'ava';
-import execa from 'execa';
+const test = require('ava');
+const execa = require('execa');
 
-test(async t => {
-	t.is(await execa.stdout('./cli.js', ['255', '154', '253']), 'ff9afd');
-	t.is(await execa.stdout('./cli.js', ['rgb(40, 42, 54)']), '282a36');
-	t.is(await execa.stdout('./cli.js', ['rgba(40, 42, 54, 75%)']), '282a36bf');
+test('assumed rgb', async t => {
+	const {stdout} = await execa('./cli.js', ['255', '154', '253']);
+	t.is(stdout, 'ff9afd');
+});
+test('rgb', async t => {
+	const {stdout} = await execa('./cli.js', ['rgb(40, 42, 54)']);
+	t.is(stdout, '282a36');
+});
+test('rgba', async t => {
+	const {stdout} = await execa('./cli.js', ['rgba(40, 42, 54, 75%)']);
+	t.is(stdout, '282a36bf');
 });
